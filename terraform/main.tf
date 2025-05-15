52e1ab47779ed3906e995822aff513509a253044
pkvac <pkvac>
2025-05-15 11:25:42 -0700

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -259,7 +259,7 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
     "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'https://www.peter-kvac.com,https://peter-kvac.com,http://localhost:5173'"
   }
 }
 
